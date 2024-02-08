4109e51a56ef0e681a09676d4fb5303d83b1b8a2
seoa <tyg04336@gmail.com>
2024-02-08 18:09:37 +0900

    탑버튼 푸터 내용추가

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 window.onload = function () {
+  // AOS적용
+  AOS.init();
   //상단 스크롤 기능
   const header = document.querySelector(".header");
   const mbt = document.querySelector(".mbt");
@@ -20,16 +22,16 @@ window.onload = function () {
   // 반응형 (1024px) 햄버거바가 생겼을때
   // 햄버거버튼을 클릭했을때
   const navMb = document.querySelector(".nav-mb");
-  const htmlRoot = document.querySelector("html")
+  const htmlRoot = document.querySelector("html");
   mbt.addEventListener("click", function () {
     const state = this.classList.contains("ani");
     if (state) {
       // 햄버거 버튼을  눌렀을때 x가 바뀌는 코드
-      this.classList.remove("ani")
+      this.classList.remove("ani");
       // 모바일에 메뉴가 나타나는 코드
-      navMb.classList.remove("active")
+      navMb.classList.remove("active");
       // 스크롤이 안생기게 하는 코드
-      htmlRoot.classList.remove("active")
+      htmlRoot.classList.remove("active");
       // // 스크롤이 되었다면
       // if (scy > 0) {
       //   header.classList.add("active");
@@ -38,21 +40,20 @@ window.onload = function () {
       //   header.classList.remove("active");
       //   mbt.classList.remove("active");
       // }
-    }else{
+    } else {
       // 햄버거 버튼을  눌렀을때 x가 바뀌는 코드
-      this.classList.add("ani")
+      this.classList.add("ani");
       // 모바일에 메뉴가 나타나는 코드
-      navMb.classList.add("active")
+      navMb.classList.add("active");
       // 스크롤이 안생기게 하는 코드
-      htmlRoot.classList.add("active")
+      htmlRoot.classList.add("active");
     }
-
   });
 
   // swiper적용
-  const swiper = new Swiper(".sw-visual",{
-    effect:"fade",
-    loop:true,
+  const swiper = new Swiper(".sw-visual", {
+    effect: "fade",
+    loop: true,
     // 슬라이드의 모션 속도를 transition 맞춘다.
     speed: 1500,
     autoplay: {
@@ -66,5 +67,53 @@ window.onload = function () {
       nextEl: ".sw-visual-next",
       prevEl: ".sw-visual-prev",
     },
-  })
+  });
+
+  // besiness 스와퍼 적용
+  const swBusiness = new Swiper(".sw-business", {
+    breakpoints: {
+      slidesPerView: 1,
+      640: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  // =======위로가기 버튼 기능
+  const gotop = document.querySelector(".gotop");
+  gotop.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+
+  let footer = document.querySelector(".footer");
+  let footerY = footer.offsetTop;
+  // console.log(footerY);
+  let waypoint_footer = new Waypoint({
+    element: document.querySelector(".footer"),
+    handler: function (direction) {
+      if (direction === "down") {
+        gotop.classList.add("active-footer");
+      }
+      else{
+        gotop.classList.remove("active-footer");
+      }
+    },
+    offset: "95%",
+  });
+  let waypoint_service = new Waypoint({
+    element: document.querySelector(".service"),
+    handler: function (direction) {
+      if (direction === "down") {
+        gotop.classList.add("active");
+      } else {
+        gotop.classList.remove("active");
+      }
+    },
+    offset: "80%",
+  });
 };
